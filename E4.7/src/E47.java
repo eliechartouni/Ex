43df70c4cd7ec29a15d7e09fdb2c01a4f3a86229
eliechartouni <eliechartouny11@hotmail.com>
2017-02-04 13:53:26 +0100

    Update E47.java

--- E4.7/src/E47.java
+++ E4.7/src/E47.java
@@ -5,34 +5,50 @@ public class E47 {
 	static int deuxiemenb[] = new int[10];
 	static int resultat[] = new int[10];
 	public static void main(String[] args) {
-		
+
 		CreateQuiz();
 		Result();
 		Score();
 	}
-	
+
 	public static void CreateQuiz(){
 		System.out.println("Welcome to Addition Quiz !");
-		
+
 		for(int i=0;i<10;i++){
 			premiernb[i] = (int)(Math.random()*50) + 1;
 			deuxiemenb[i] = (int)(Math.random()*50) + 1;
 		}
 	}
 	public static void Result(){
-		
+
 		Scanner stdin = new Scanner( System.in );
+
 		for(int i = 0;i<10; i++){
-			System.out.println("Question "+ (i+1) +" : What is "+premiernb[i]+" + "+deuxiemenb[i]+" ?");
-			
-			resultat[i] = stdin.nextInt();
+			int k=1;
+			while (k==1){
+				try{
+
+					System.out.println("Question "+ (i+1) +" : What is "+premiernb[i]+" + "+deuxiemenb[i]+" ?");
+					stdin = new Scanner( System.in );
+					resultat[i] = stdin.nextInt();
+					k=0;
+				}
+				catch(Exception e){
+					System.out.println("La valeur saisie n'est pas un entier. Veuillez saisir une valeur entiere");
+
+				}
+
+			}
 		}
+		stdin.close();
+
 	}
+
 	public static void Score(){
 		int count =0;
 		for(int i = 0;i<10; i++){
 			int res = premiernb[i]+ deuxiemenb[i];
-			System.out.print("Question "+ (i+1) +": "+premiernb[i]+" + "+deuxiemenb[i]+"="+res+" ");
+			System.out.print("Question "+ (i+1) +": "+premiernb[i]+" + "+deuxiemenb[i]+" = "+resultat[i]+" ");
 			if(res == resultat[i]){
 				count++;
 				System.out.println("Your answer is right");
