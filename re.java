fac523bb386ee4aaa7dd167f28452566f29bb1b2
eliechartouni <eliechartouny11@hotmail.com>
2017-03-16 14:07:33 +0100

    Update re.java

--- re.java
+++ re.java
@@ -13,26 +13,29 @@ public class re {
 		try{
 			String Tabl[] = {"Last Name", "First Name","Title","Year"};
 			String tr = "";
+			String x = "";
 			
 			Scanner input = new Scanner(System.in);
 			
 			while(input.hasNext()){
 				for ( int i = 0; i < Tabl.length; i++ ) {
-					
+					x = input.nextLine();
 					System.out.print("Enter" + Tabl[i] + " :");
 					
 					if (tr != null){
-					tr = tr + ";"+ input.nextLine();
+					tr = tr + ";"+ x;
 					}
-					else {tr = input.nextLine();}
+					else {tr = x;}
 				}
+				input.close();
+				break;
 			}
 		
 		String fil = "/users/charel16/workspace-mars-2015/Library_Books";
 		FileWriter wr = new FileWriter(fil.trim().toString());
 		wr.write(tr);
+		wr.write(\n);
 		wr.close();
-		input.close();
 	}
 		catch(Exception e){
 			System.out.println(e.getMessage());
